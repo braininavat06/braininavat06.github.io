dc59fc0ae8ba6c6b01d96955502cc23bfb5538b5
braininavat06 <141223411+braininavat06@users.noreply.github.com>
2024-09-21 10:06:09 +0900

    Update htmlModule.js

--- js/htmlModule.js
+++ js/htmlModule.js
@@ -4,7 +4,6 @@ var htmlModule = {
 "head" : 
 `
 <meta charset="UTF-8">
-<meta name="google-site-verification" content="dcfGFxfGYi6Pi5YZ5UHZmw_Oj305RJf5sbbc7uSR8K0" />
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
