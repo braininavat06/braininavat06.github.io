bba3179f63b7236a4ec0857b48a23ba899708ee9
braininavat06 <thddlsckd06@gmail.com>
2025-03-17 01:51:28 +0000

    sw 레지스터 수동으로 변경

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://braininavat06.github.io/sw.js', { scope: 'https://braininavat06.github.io/' })})}
